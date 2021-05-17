20b623ee251e03214452aa12905c96baf1575b58
watanabe210308 <83203349+watanabe210308@users.noreply.github.com>
2021-05-17 14:04:33 +0900

    Create input.php

---
+++ html/input.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>memo-app</title>
+  </head>
+  <body>
+    <h1>メモアプリ</h1>
+    <form action="post.php" method="post"> 
+      <textarea id="memo" name="memo" cols="30" rows="10"> </textarea>
+      <button type="submit">送信</button>
+    </form>
+  </body>
+</html>
